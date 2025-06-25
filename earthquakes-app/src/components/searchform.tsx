6b37cb223a21e4e581d43213290d2a5d32bdf15c
Jenny Huang <Jenny@imac.attlocal.net>
2025-06-25 18:44:53 -0400

    Stack vertically search input for mobile

--- earthquakes-app/src/components/searchform.tsx
+++ earthquakes-app/src/components/searchform.tsx
@@ -13,36 +13,36 @@ export default function SearchForm() {
           }}
           className="flex flex-col gap-4 mb-8 items-center"
         >
-            <div className="flex w-full max-w-2xl gap-2">
-            <input
+            <div className="flex w-full max-w-xs gap-2 flex-col sm:flex-row sm:max-w-2xl">
+              <input
               name="startYear"
               type="number"
               min="1900"
               max={new Date().getFullYear()}
               placeholder="Start year"
               required
-              className="w-35 px-4 py-2 border border-red-400 rounded-md focus:outline-none focus:ring-2 focus:ring-red-400"
-            />
-            <span className="self-center text-gray-600">-</span>
-            <input
-              name="endYear"
-              type="number"
-              min="1900"
-              max={new Date().getFullYear()}
-              placeholder="End year"
-              required
-              className="w-35 px-4 py-2 border border-red-400 rounded-md focus:outline-none focus:ring-2 focus:ring-red-400"
-            />
-            <input
-              name="minMagnitude"
-              type="number"
-              min="0"
-              step="0.1"
-              placeholder="Min magnitude"
-              required
-              className="flex-1 px-4 py-2 border border-red-400 rounded-md focus:outline-none focus:ring-2 focus:ring-red-400"
-            />
-            <SearchButton />
+              className="w-35 px-4 border border-red-400 rounded-md focus:outline-none focus:ring-2 focus:ring-red-400 sm:py-2"
+              />
+              <span className="self-center text-gray-600 hidden sm:flex">-</span>
+              <input
+                name="endYear"
+                type="number"
+                min="1900"
+                max={new Date().getFullYear()}
+                placeholder="End year"
+                required
+                className="w-35 px-4 border border-red-400 rounded-md focus:outline-none focus:ring-2 focus:ring-red-400 sm:py-2"
+              />
+              <input
+                name="minMagnitude"
+                type="number"
+                min="0"
+                step="0.1"
+                placeholder="Min magnitude"
+                required
+                className="flex-1 px-4 py-2 border border-red-400 rounded-md focus:outline-none focus:ring-2 focus:ring-red-400"
+              />
+              <SearchButton />
             </div>
         </form>
   );
